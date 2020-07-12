6966a0a99b0b0f8c6bd414ac07922e05efee8094
Mike Polostyuk <lynx_online@mail.ru>
2020-07-12 13:24:34 +0300

    update

--- Sockets_client_RASPBERRY.py
+++ Sockets_client_RASPBERRY.py
@@ -3,23 +3,50 @@
 #%%
 import socket
 
+def get_text(receiving_socket):
+    buffer = ""
+
+    socket_open = True
+    while socket_open:
+        # read any data from the socket
+        data = receiving_socket.recv(1024)
+        '''The data is received or read from the client_socket using client_socket.recv(1024). 
+        The value 1024 is the maximum number of bytes that should be read at one time. 
+        If more than 1,024 bytes had been sent, subsequent calls to recv would receive the rest of the data.'''
+
+        # if no data is returned the socket must be closed
+        if not data:
+            socket_open = False
+
+        # add the data to the buffer
+        buffer = buffer + data.decode()
+        '''The data received by the socket is a stream of bits and needs to be decoded to a string using decode() 
+        so that it can be printed to the screen.'''
+
+        # is there a terminator in the buffer
+        terminator_pos = buffer.find("\n")
+        # if the value is greater than -1, a \n must exist
+        while terminator_pos > -1:
+            # get the message from the buffer
+            message = buffer[:terminator_pos]
+            # remove the message from the buffer
+            buffer = buffer[terminator_pos + 1:]
+            # yield the message (see below)
+            yield message
+            # is there another terminator in the buffer
+            terminator_pos = buffer.find("\n")
+
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(("127.0.0.1", 8081))
 print("Connected")
 
-data = client_socket.recv(1024)
-'''The data is received or read from the client_socket using client_socket.recv(1024). The value 1024 is the maximum number of bytes that should be read at one time. 
-If more than 1,024 bytes had been sent, subsequent calls to recv would receive the rest of the data.'''
-
-message = data.decode()
-'''The data received by the socket is a stream of bits and needs to be decoded to a string using decode() so that it can be printed to the screen.'''
-
-print(message)
-
-if message == 'Hello, thanks for connecting':
-    reply = "Send nudes"
-    data = reply.encode()
-    client_socket.send(data)
+for message in get_text(client_socket):
+    print(message)
+    if message == 'Hello, thanks for connecting':
+        reply = "Send nudes"
+        data = reply.encode()
+        client_socket.send(data)
 
 
 client_socket.close()
--- Sockets_server_RASPBERRY.py
+++ Sockets_server_RASPBERRY.py
@@ -2,6 +2,18 @@
 
 import socket
 
+def send_text(sending_socket, text):
+    text = text + "\n"
+    data = text.encode()
+    '''The encode() function uses the character encoding standard utf-8 to encode each character in the message string 
+    variable into a series of bytes. You can encode your data using different encoding standards, 
+    for example message.encode("base64"), which is useful when dealing with non-text data.'''
+
+    sending_socket.send(data)
+    '''The connection_socket is the socket that is paired and connected to the client socket. 
+    Note that this is different to the server_socket, which is used to listen for and accept connections, not use them.'''
+
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 '''The parameters socket.AF_INET and socket.SOCK_STREAM determine the type of socket and protocol to be used.
 In this case, AF_INET specifies Internet Protocol version 4 and SOCK_STREAM denotes TCP, so a TCP/IP socket is created.'''
@@ -19,14 +31,7 @@ connection_socket, address = server_socket.accept()
 print("Client connected")
 
 message = "Hello, thanks for connecting"
-data = message.encode()
-'''The encode() function uses the character encoding standard utf-8 to encode each character in the message string 
-variable into a series of bytes. You can encode your data using different encoding standards, 
-for example message.encode("base64"), which is useful when dealing with non-text data.'''
-
-connection_socket.send(data)
-'''The connection_socket is the socket that is paired and connected to the client socket. 
-Note that this is different to the server_socket, which is used to listen for and accept connections, not use them.'''
+send_text(connection_socket, message)
 
 data2 = connection_socket.recv(1024)
 message2 = data2.decode()

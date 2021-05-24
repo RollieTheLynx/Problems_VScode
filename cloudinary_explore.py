27bdd642662e0bb7abe3abd394c65e93264ad1d8
Mike Polostyuk <lynx_online@mail.ru>
2021-05-24 10:40:03 +0300

    cloudinary

---
+++ cloudinary_explore.py
@@ -0,0 +1,21 @@
+# https://cloudinary.com/documentation/django_integration
+# https://cloudinary.com/documentation/image_upload_api_reference
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+cloudinary.config( 
+  cloud_name = "dq0j8nvsz", 
+  api_key = "429268596266338", 
+  api_secret = "fgzjSTei_Uevjc77AGwt8X0-JXI" 
+)
+
+cloudinary.uploader.upload("C:\\Users\\TN90072\\Pictures\\My Post-1.jpg", 
+  folder = "Germany", 
+  #public_id = "APIied",
+  overwrite = 'true', 
+  use_filename = 'true',
+  unique_filename = 'false',
+  #notification_url = "https://mysite.example.com/notify_endpoint", 
+  resource_type = "image")

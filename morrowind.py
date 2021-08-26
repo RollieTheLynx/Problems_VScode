d2fe19af40b81aba53c726e02fa9a94bf180bb30
RollieTheLynx <lynx_online@mail.ru>
2021-08-26 19:52:43 +0300

    morro

--- morrowind.py
+++ morrowind.py
@@ -5,6 +5,9 @@ https://github.com/hmi-utwente/video-game-text-corpora
 """
 import json
 with open('imperial_library_20200626b.json') as f:
-  data = json.load(f)
+    data = json.load(f)
 
-print(data["https://www.imperial-library.info/content/dying-mans-last-words"]['text'])
+for key in data:
+    if "Morrowind" in data[key]["game"]:
+        print(data[key]["title"])
+        print(data[key]["text"])

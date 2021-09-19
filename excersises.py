eccf971bbaa6bcafb1ab7c4888bc25494074b748
RollieTheLynx <lynx_online@mail.ru>
2021-09-19 13:22:17 +0300

    removed django playground

--- excersises.py
+++ excersises.py
@@ -2,7 +2,10 @@
 """
 
 """
-def test(a=1, b=3):
-    return print(a+b)
+import dj_database_url
 
-test(78, 77)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default='postgres://sljaiyeemkopbo:80b6d99c0dff13e1fc7ce32385d90963fea9d9065dad402e07d063fdbcb1d927@ec2-34-253-116-145.eu-west-1.compute.amazonaws.com:5432/d4okmrqc4lb2mh')
+DATABASES['default'] = dj_database_url.parse('postgres://sljaiyeemkopbo:80b6d99c0dff13e1fc7ce32385d90963fea9d9065dad402e07d063fdbcb1d927@ec2-34-253-116-145.eu-west-1.compute.amazonaws.com:5432/d4okmrqc4lb2mh', conn_max_age=600)
+print(DATABASES['default'])
--- mikesplayground_django/pages/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ApiExchange, CatPhoto
-
-# Register your models here.
-admin.site.register(ApiExchange)
-admin.site.register(CatPhoto)

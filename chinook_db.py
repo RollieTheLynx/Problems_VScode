a6a9eb22e47efc8d05c9fe7f9243e6d45704ac72
RollieTheLynx <lynx_online@mail.ru>
2021-10-18 22:01:37 +0300

    chinook

--- chinook_db.py
+++ chinook_db.py
@@ -16,10 +16,12 @@ import sqlite3
 conn = sqlite3.connect('chinook.db')
 cur = conn.cursor()
 
+## все записи
 # query = "SELECT * FROM albums;"
 # results = cur.execute(query).fetchall()
 # print(results)
 
+## поиск по условиям
 # query = '''SELECT
 #                 name,
 #                 milliseconds,
@@ -34,6 +36,7 @@ cur = conn.cursor()
 # results = cur.execute(query).fetchall()
 # print(results)
 
+## неточное соответствие
 # query = '''SELECT
 #                 name,
 #                 albumid,
@@ -49,6 +52,7 @@ cur = conn.cursor()
 # results = cur.execute(query).fetchall()
 # print(results)
 
+## JOIN
 # query = '''SELECT 
 #                 Name,
 #                 Title
@@ -61,6 +65,7 @@ cur = conn.cursor()
 # results = cur.execute(query).fetchall()
 # print(results)
 
+## два JOIN
 # query = '''SELECT
 #                 tracks.name AS track,
 #                 albums.title AS album,
@@ -71,27 +76,46 @@ cur = conn.cursor()
 #                 albums ON albums.albumid = tracks.albumid
 #             INNER JOIN
 #                 artists ON artists.artistid = albums.artistid
-#             LIMIT
-#                 20;'''
+#             WHERE
+#                 track = 'The Trooper';'''
 # results = cur.execute(query).fetchall()
 # print(results)
 
-query = '''SELECT
-                tracks.name AS track,
-                albums.title AS album,
-                artists.name AS artist
-            FROM
-                tracks
-            INNER JOIN
-                albums ON albums.albumid = tracks.albumid
-            INNER JOIN
-                artists ON artists.artistid = albums.artistid
-            WHERE
-                track = 'The Trooper';'''
+## суммирование (MIN, MAX, SUM, COUNT, AVG)
+# query = '''SELECT
+#                 albums.title AS album,
+#                 COUNT(trackid)
+#             FROM
+#                 tracks
+#             INNER JOIN
+#                 albums ON albums.albumid = tracks.albumid
+#             GROUP BY
+#                 album;'''
+# results = cur.execute(query).fetchall()
+# print(results)
+
+## GROUP BY date
+# query = '''SELECT
+#    STRFTIME('%Y', InvoiceDate) InvoiceYear, 
+#    COUNT(InvoiceId) InvoiceCount
+# FROM
+#    invoices
+# GROUP BY
+#    STRFTIME('%Y', InvoiceDate)
+# ORDER BY
+#    InvoiceYear;'''
+# results = cur.execute(query).fetchall()
+# print(results)
+
+# несколько запросов в одну выдачу
+query = '''SELECT FirstName, LastName, 'Employee' AS Type
+            FROM employees
+            UNION
+            SELECT FirstName, LastName, 'Customer'
+            FROM customers
+            ORDER BY FirstName, LastName;'''
 results = cur.execute(query).fetchall()
 print(results)
-
-
 
 conn.close()
 
